d0238c3b9919d3fb2af58f40ac30052fb028cd2d
ddd503 <skbsporta7@gmail.com>
2019-05-02 13:27:16 +0900

    Initial Commit

---
+++ CollectionView-Header-Footer-Sample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CollectionView-Header-Footer-Sample
+//
+//  Created by kawaharadai on 2019/05/02.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
